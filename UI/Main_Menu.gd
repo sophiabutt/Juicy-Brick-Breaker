88fef8ea22f782c575db165772df7757e1b753aa
sophiabutt <buttso@iu.edu>
2023-10-11 17:00:02 -0400

    part 1

--- UI/Main_Menu.gd
+++ UI/Main_Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_Play_pressed():
-	get_tree().change_scene_to_file("res://Game.tscn")
+	get_tree().change_scene_to_file("res://game.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
